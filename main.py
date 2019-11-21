9f045f0c3ca07eb8303b96218a8384a27f905264
Sudip Mitra <44464604+sudipmitraonline@users.noreply.github.com>
2019-11-21 22:57:35 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+#@Author : Sudip Mitra
+#Email : sudipmitraonline@gmail.com
+def split_and_join(line):
+    return line.replace(" ","-")
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
